01035c842a2a462d835a1f0c29ea304164d89070
deb80137 <64010282+deb80137@users.noreply.github.com>
2020-04-25 12:30:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-state=1"/>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css">
 	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.0/js/bootstrap.min.js"></script>
 
 	<style type="text/css">
 	.bg{background: url('https://jalapps.herokuapp.com/back.jpg') ;
