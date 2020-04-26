d322bbf7a5342796314578f172997b3f49f62378
deb80137 <64010282+deb80137@users.noreply.github.com>
2020-04-26 12:38:12 +0530

    Update logincheck.php

--- apps/logincheck.php
+++ apps/logincheck.php
@@ -2,12 +2,7 @@
 //error_reporting(0);
 session_start();
 include 'ServerDetails.php';
-
-$url=parse_url(getenv("us-cdbr-iron-east-01.cleardb.net"));
-	$host = $url1["us-cdbr-iron-east-01.cleardb.net"];
-	$username=$url["b7fdf2e2fbe34d"]; // Mysql username 
-	$password=$url["4e878331"]; // Mysql password 
-	$db_name=substr($url["heroku_ccdd37a9508be9a"],1); // Database name 
+include 'dbconfig.php';
 $conn=mysql_connect("$host", "$username", "$password")or die("cannot connect");
 $myusername=$_POST['myusername']; 
 $mypassword=sha1($_POST['mypassword']);
