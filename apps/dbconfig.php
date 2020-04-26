96b0f6b76d77e7bdbccb9d3264fce58e176231ce
deb80137 <64010282+deb80137@users.noreply.github.com>
2020-04-26 11:59:02 +0530

    Update dbconfig.php

--- apps/dbconfig.php
+++ apps/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
-	$host="localhost"; // Host name 
-	$username="root"; // Mysql username 
-	$password="star@#1911"; // Mysql password 
+	$host="us-cdbr-iron-east-01.cleardb.net"; // Host name 
+	$username="b7fdf2e2fbe34d"; // Mysql username 
+	$password="4e878331"; // Mysql password 
 	$db_name="heroku_ccdd37a9508be9a"; // Database name 
 ?>
