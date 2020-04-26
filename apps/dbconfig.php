3059248bc216539e25f8157dfea7c60af72f80b7
deb80137 <64010282+deb80137@users.noreply.github.com>
2020-04-26 13:57:51 +0530

    Update dbconfig.php

--- apps/dbconfig.php
+++ apps/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 	$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
-	$server = $url["host1"];
+	$server = $url["host"];
 	$username=$url["user"]; // Mysql username 
 	$password=$url["pass"]; // Mysql password 
 	$db=substr($url["path"],1); // Database name 
