400e7b58350310ff26f683cdd82568ceb3506be9
deb80137 <64010282+deb80137@users.noreply.github.com>
2020-04-26 14:03:46 +0530

    Update index.php

--- apps/index.php
+++ apps/index.php
@@ -42,7 +42,7 @@ box-shadow: -5px 2px 10px 6px rgba(0,0,0,0.75);
 		<div class="row">
 			<div class="col-md-4 col-sm-4 col-xs-12"></div>
 						<div class="col-md-4 col-sm-4 col-xs-12 text-center">
-							<form id="log" action="logincheck1.php" method="post">
+							<form id="log" action="logincheck.php" method="post">
 								<h1>Login Form</h1>
 								<img class="rounded-circle text-center" src="https://jalapps.herokuapp.com/apps/img/login.jpg">
 								<div class="form-group">
