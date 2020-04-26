e500eca5db61ff68356cbe6cb06ea03204006ba1
deb80137 <64010282+deb80137@users.noreply.github.com>
2020-04-26 12:42:43 +0530

    Update logincheck.php

--- apps/logincheck.php
+++ apps/logincheck.php
@@ -3,7 +3,7 @@
 session_start();
 include 'ServerDetails.php';
 include 'dbconfig.php';
-$conn=mysql_connect("$host", "$username", "$password")or die("cannot connect");
+$conn=mysqli_connect("$host", "$username", "$password")or die("cannot connect");
 $myusername=$_POST['myusername']; 
 $mypassword=sha1($_POST['mypassword']);
 
