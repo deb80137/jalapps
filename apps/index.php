1553c316135d91ac8e7c3fcbfb6e07c95abd8cec
deb80137 <64010282+deb80137@users.noreply.github.com>
2020-04-26 11:10:13 +0530

    Update index.php

--- apps/index.php
+++ apps/index.php
@@ -51,7 +51,7 @@ box-shadow: -5px 2px 10px 6px rgba(0,0,0,0.75);
 									
 								</div>
 								<div class="form-group">
-									<label></label><input type="password" name="mypassword" id="mypassword" class="form-control" placeholder="Password">						
+									<input type="password" name="mypassword" id="mypassword" class="form-control" placeholder="Password">						
 								</div>
 								<div class="form-group">
 									<label></label>						
