c2f9caecd747370a51ed6afe6f6e599aad10b900
deb80137 <64010282+deb80137@users.noreply.github.com>
2020-04-26 12:39:05 +0530

    Update dbconfig.php

--- apps/dbconfig.php
+++ apps/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 	$url=parse_url(getenv("us-cdbr-iron-east-01.cleardb.net"));
-	$host = $url["host"];
+	$host = $url["us-cdbr-iron-east-01.cleardb.net"];
 	$username=$url["b7fdf2e2fbe34d"]; // Mysql username 
 	$password=$url["4e878331"]; // Mysql password 
 	$db_name=substr($url["heroku_ccdd37a9508be9a"],1); // Database name 
